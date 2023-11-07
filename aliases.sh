ac2a588a797bceb38cfb63bb7ff0a250abe3ef75
danielosullivan2007 <danielosullivan2007@GMAIL.COM>
2023-11-07 20:12:16 +0000

    Added script to add aliases to bashrc/bash_profile
    ToDo:Automate adding all the scripts in the folder

---
+++ aliases.sh
@@ -0,0 +1,18 @@
+
+check_and_set () {
+
+for var in "$@"
+do
+    if grep -q $var ~/.bashrc ~/.bash_profile; then
+    echo "Alias $var already exists"
+else
+    echo "Adding alias $var"
+    echo "alias $var=$(pwd)/$var.sh" | tee -a ~/.bashrc ~/.bash_profile
+fi
+done
+
+}
+
+
+check_and_set find_between archive new_git_repo
+echo "Don't forget to run 'exec bash' to reset terminal"
